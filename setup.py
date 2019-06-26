53dbb77669b315601127c96898a728fdc9dfa2ad
PaoloSini <sinipaolo@gmail.com>
2019-06-26 10:50:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,5 @@
 from setuptools import setup, find_packages
 
 setup(name='custom_cabot_alert_slack',
-      #version='0.8.3',
       description='A slack alert plugin for Cabot by Arachnys',
-      author='Luka Blaskovic',
-      author_email='lblasc@znode.net',
-      url='http://lblasc.znode.net',
-      packages=find_packages(),
-      download_url = 'https://github.com/lblasc/cabot-alert-slack/tarball/0.6',
      )
